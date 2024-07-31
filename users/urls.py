11ee341b70c581af9f7a8111bc020245eeff7514
Chebor <cheborbrighton805@gmail.com>
2024-07-31 18:53:14 +0300

    Added signup and login apis

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_api, name='login'),
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test_token', views.test_token),
 ]
