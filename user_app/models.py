22b8f3d240a8e5694c23e83c6411cde188f3ac56
junior <lewisomogi01@gmail.com>
2024-08-04 20:57:14 -0700

    working on models

--- user_app/models.py
+++ user_app/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
