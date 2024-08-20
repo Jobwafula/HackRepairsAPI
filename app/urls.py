69f330c47c4997259ddfcb3b3f577dcbd4fee01b
Chebor <cheborbrighton805@gmail.com>
2024-08-20 14:42:44 +0300

    cn

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'app'
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="app/index.html")),
-]
